787aee9ee80f3c3485a327899c5909a7520b8a59
Rafael Müller <rafael.mueller@scs.ch>
2023-09-23 21:43:14 +0200

    Fix: Add main

---
+++ app/src/main.py
@@ -0,0 +1,9 @@
+import sys
+
+sys.path.append('/workspaces/wedding-api/app')
+
+from src.routes.v1 import app_v1
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app=app_v1, host='0.0.0.0', port=8000)
