a6ac6ff71cadeb47c38bb8a33cc260fb9f5b488f
Rafael Müller <rafael.mueller@scs.ch>
2023-09-23 21:43:45 +0200

    Fix: Change database location

--- app/src/database/db.py
+++ app/src/database/db.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker, Session
 
-engine = create_engine("sqlite:///test.db", echo=True, connect_args={"check_same_thread": False})
+engine = create_engine("sqlite:///data/db.sqlite", echo=True, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
