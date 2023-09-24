ee94ec55bb8d0ecfd6532f0614e43f701bfa9f38
Rafael Müller <rafael.mueller@scs.ch>
2023-09-24 08:42:35 +0200

    Fix: Simplify invitation codes

--- app/src/setup/populate_db.py
+++ app/src/setup/populate_db.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 
     invitation_codes = {}
 
-    p = Path('./data/guests.csv')
+    p = Path('./raw/guests.csv')
     print(p.absolute())
     df = pd.read_csv(p, header=0, delimiter=';')
 
@@ -25,11 +25,14 @@ if __name__ == '__main__':
     for group_id in df['group'].unique():
         invitation_token = generate_token(8)
         inviation_hash = hash_token(invitation_token)
-        invitation_codes[inviation_hash] = invitation_token
         i = User(invitation_hash=inviation_hash, status=0, email_verification_hash=None, last_login=None, email=None, password_hash=None)
 
+        names = []
         for id, row in df.loc[df['group'] == group_id, :].iterrows():
             i.associated_guests.append(Guest(first_name=row.first_name, last_name=row.last_name, status=0, food_option=0, allergies=''))
+            names.append(row.first_name)
+
+        invitation_codes[inviation_hash] = (invitation_token, ", ".join(names))
 
         session.add(i)
 
