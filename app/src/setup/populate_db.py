5edf4b889cc58bbfe56a6e19ea7d9ffcfb9074c1
Rafael Müller <rafael.mueller@scs.ch>
2023-09-29 20:00:09 +0000

    Feat: Generate QR code inviations

--- app/src/setup/populate_db.py
+++ app/src/setup/populate_db.py
@@ -1,11 +1,13 @@
 import sys
 import pandas as pd
 from pathlib import Path
+import json
 
 sys.path.append('/workspaces/wedding-api/app')
 
 from src.security import generate_token, hash_token
 from src.database.models import User, Guest
+from src.setup.qr_code import QrCodeImageGenerator
 
 from src.database.db import Base, engine, SessionLocal
 
@@ -18,8 +20,13 @@ if __name__ == '__main__':
     print(p.absolute())
     df = pd.read_csv(p, header=0, delimiter=';')
 
+    base_url = 'https://homepage-beta.molitoris.org/registration?code='
+    output_base_path = Path(__file__).parent.joinpath('../../../data/qr_codes')
+    output_base_path.mkdir(parents=True)
 
     session = SessionLocal()
+
+    qr_generator = QrCodeImageGenerator()
 
 
     for group_id in df['group'].unique():
@@ -32,6 +39,8 @@ if __name__ == '__main__':
             i.associated_guests.append(Guest(first_name=row.first_name, last_name=row.last_name, status=0, food_option=0, allergies=''))
             names.append(row.first_name)
 
+        qr_generator.get_image(text=base_url + invitation_token, output_path=output_base_path.joinpath('_'.join(names)).with_suffix('.png'))
+
         invitation_codes[inviation_hash] = (invitation_token, ", ".join(names))
 
         session.add(i)
@@ -39,4 +48,5 @@ if __name__ == '__main__':
     session.commit()
     session.close()
 
-    print(invitation_codes)
+    with open(output_base_path.joinpath('../hash_lookup.txt'), 'w') as f:
+        f.write(json.dumps(invitation_codes, indent=2, ensure_ascii=False))
