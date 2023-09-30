2f80021dfec8b09727947e188ab0ec4c51e6c446
Rafael Müller <rafael.mueller@scs.ch>
2023-09-30 19:24:41 +0000

    Feat: Add user roles

--- app/src/database/models.py
+++ app/src/database/models.py
@@ -1,27 +1,46 @@
 import sys
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, Session, relationship
+from sqlalchemy import Table, Column, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import List
 
 
 sys.path.append('/workspaces/wedding-api/app')
 
 from src.database.db import Base
-from src.security import generate_token
+
+user_role_table = Table(
+    'user_role_table',
+    Base.metadata,
+    Column('role_id', ForeignKey('role_table.id')),
+    Column('user_id', ForeignKey('user_table.id'))
+)
+
+
+class Role(Base):
+    __tablename__ = 'role_table'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str]
 
 
 class User(Base):
     __tablename__ = 'user_table'
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_name: Mapped[str] = mapped_column(nullable=True)
     email: Mapped[str] = mapped_column(unique=True, nullable=True)
     password_hash: Mapped[str] = mapped_column(nullable=True)
     invitation_hash: Mapped[str] = mapped_column(index=True, unique=True, nullable=False)
     email_verification_hash: Mapped[str] = mapped_column(index=True, unique=True, nullable=True)
     last_login: Mapped[str] = mapped_column(nullable=True)
     status: Mapped[int]
-    # Relationship
+    # Relationships
+
+    # 1:n = User : Guest
     associated_guests: Mapped[List["Guest"]] = relationship(back_populates="user")
+    
+    # m:n = User:Role
+    role: Mapped[List['Role']] = relationship(secondary=user_role_table)
 
 
 class Guest(Base):
@@ -36,15 +55,3 @@ class Guest(Base):
     # Relationship
     user_id: Mapped[int] = mapped_column(ForeignKey("user_table.id"))
     user: Mapped["User"] = relationship(back_populates="associated_guests")
-
-
-
-
-# with Session(engine) as session:
-#     i = Invitation(invitation_hash=generate_code(16))
-
-#     i.associated_guests.append(Guest(first_name='Rafael', last_name='Müller'))
-#     i.associated_guests.append(Guest(first_name='Melanie', last_name='Häner'))
-    
-#     session.add(i)
-#     session.commit()
