809bfeef40e4e12f225c75b14bd8d186cdad0137
Rafael Müller <rafael.mueller@scs.ch>
2023-11-07 20:31:12 +0000

    Fix: Move DTO to separate folder

---
+++ app/src/routes/dto.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+class RegistrationData(BaseModel):
+    invitation_token: str
+    email: str
+    password: str
+
+class LoginData(BaseModel):
+    email: str
+    password: str
+
+class EmailVerificationDate(BaseModel):
+    token: str
+
+
+class Guest(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    food_option: int
+    allergies: str
