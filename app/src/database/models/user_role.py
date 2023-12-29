93bff5ada0a1513e72ad68ee34b511c1e928bcb2
Rafael Müller <rafael.mueller@scs.ch>
2023-12-29 09:05:05 +0000

    Feat: Add questions to form

--- app/src/database/models/user_role.py
+++ app/src/database/models/user_role.py
@@ -2,6 +2,6 @@ from enum import Enum
 
 
 class UserRole(Enum):
-    GUEST = 1,
-    WITNESS = 2,  # Witnesses can be contacted
-    ADMIN = 3,
+    GUEST = 1
+    WITNESS = 2  # Witnesses can be contacted
+    ADMIN = 3
